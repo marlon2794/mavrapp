b1d6ac2f9408463163fbef633546756ef27df20f
server <marlonapolop2794@gmail.com>
2020-11-13 10:03:44 -0800

    Creacion de ruta lucky number y rendereizacion con twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -16,12 +16,9 @@ class LuckyController extends AbstractController
     */
     public function number(): Response
     {
-        /*$number = random_int(0, 100);
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );*/
-        $number = random_int(0,1);
-        return $this-> render('lucky/number.html.twig', [
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
             'number' => $number,
         ]);
     }
