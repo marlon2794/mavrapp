a0d4599b1bb91669aa8f59625b25fb25b7af7a52
server <marlonapolop2794@gmail.com>
2020-11-13 09:43:54 -0800

    Configuracion con twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,17 +4,25 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class LuckyController
+
+// Library para poder utilizar el renderizado de plantilas HTML con Twig
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// La clase debe heredad de AbstractController para que funcionen las rutas
+class LuckyController extends AbstractController
 {
     /**
     * @Route("/lucky/number")
     */
     public function number(): Response
     {
-        $number = random_int(0, 100);
-
+        /*$number = random_int(0, 100);
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        );*/
+        $number = random_int(0,1);
+        return $this-> render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
