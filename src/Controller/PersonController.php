03802d56fb0059f750fcb454108d08740167dc92
server <marlonapolop2794@gmail.com>
2020-11-15 12:20:49 -0800

    Conexion base de datos

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -13,8 +13,21 @@ class PersonController extends AbstractController
      */
     public function index(): Response
     {
+        $db = parse_url(getenv("DATABASE_URL"));
+
+        $pdo = new PDO("pgsql:" . sprintf(
+            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+            $db["host"],
+            $db["port"],
+            $db["user"],
+            $db["pass"],
+            ltrim($db["path"], "/")
+        ));
+
+        $var = $db["host"];
+
         return $this->render('person/index.html.twig', [
-            'controller_name' => 'Person Controller',
+            'controller_name' => $var,
         ]);
     }
 }
